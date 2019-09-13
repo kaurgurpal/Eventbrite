1e1d83bde4284cca25e74ed5e2b78ed58c08fc05
Leela Murali Yegireddi <leelamurali.yegireddi@gmail.com>
2019-09-13 11:57:00 -0700

    Added classes to create tables  for EventCatalogAPI

---
+++ EventCatalog/Domain/EventCategory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventCatalog.Domain
+{
+    public class EventCategory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
---
+++ EventCatalog/Domain/EventsCatalog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventCatalog.Domain
+{
+    public class EventsCatalog
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public decimal Price { get; set; }
+
+        //ForiegnKey relation with EventType table
+        public int EventTypeId { get; set; }
+        public virtual EventType EventType { get; set; }
+
+        //ForiegnKey relation with EventCategory table
+        public int EventCategoryId { get; set; }
+        public virtual EventCategory EventCategory { get; set; }
+
+        //ForiegnKey relation with Location table
+        public int LocationId { get; set; }
+        public virtual Location Location { get; set; }
+
+        //ForiegnKey relation with Organizer table
+        public int OrganizerId { get; set; }
+        public virtual Organizer Organizer { get; set; }
+    }
+}
