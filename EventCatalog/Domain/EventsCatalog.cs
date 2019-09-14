162a0cec6d43dfb81b8239cc478ff76ce4f9e49f
sstambat12 <51150967+sstambat12@users.noreply.github.com>
2019-09-14 00:09:04 -0700

    Created classes for EventCatalogAPI.

---
+++ EventCatalog/Domain/EventsCatalog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventCatalogAPI.Domain
+{
+    public class EventsCatalog
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDateTime { get; set; }
+        public DateTime EndDateTime { get; set; }
+        public string EventImageUrl { get; set; }
+        public string OrganizerName { get; set; }
+        public string OrganizerDescription { get; set; }
+        
+        //setting up Foreign key relation with EventCategory
+        public int EventCategoryId { get; set; }
+        public virtual EventCategory EventCategory { get; set; }
+
+        ////setting up Foreign key relation with EventType
+        public int EventTypeId { get; set; }
+        public virtual EventType EventType { get; set; }
+
+        //setting up Foreign key relation with EventLocation
+        public int EventLocationId { get; set; }
+        public virtual EventLocation EventLocation { get; set; }
+
+    }
+}
---
+++ EventCatalog/Migrations/20190913061451_Initial.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EventCatalog.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateSequence(
+                name: "Event_Types_hilo",
+                incrementBy: 10);
+
+            migrationBuilder.CreateTable(
+                name: "EventTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventTypes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EventTypes");
+
+            migrationBuilder.DropSequence(
+                name: "Event_Types_hilo");
+        }
+    }
+}
