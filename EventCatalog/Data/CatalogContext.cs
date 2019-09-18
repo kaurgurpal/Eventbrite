c09188b764ad6b986e45d9ce3f3e6ed080dea456
Gurpal Kaur <gurpal.kaur@outlook.com>
2019-09-18 00:39:52 -0700

    Added Domain Classes and Data Context
    Seeding in progress

---
+++ EventCatalog/Data/CatalogContext.cs
@@ -0,0 +1,89 @@
+﻿using EventCatalogAPI.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventCatalogAPI.Data
+{
+    public class CatalogContext:DbContext
+    {
+        public CatalogContext(DbContextOptions options):base(options)
+        { }
+
+        public DbSet<CatalogCategory> CatalogCategories { get; set; }
+        public DbSet<CatalogType> CatalogTypes { get; set; }
+        public DbSet<CatalogEvent> CatalogEvents { get; set; }
+        public DbSet<EventLocation> EventLocations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CatalogCategory>(ConfigureCatalogCategory);
+            modelBuilder.Entity<CatalogType>(ConfigureCatalogType);
+            modelBuilder.Entity<CatalogEvent>(ConfigureCatalogEvent);
+            modelBuilder.Entity<EventLocation>(ConfigureEventLocation);
+
+        }
+
+        private void ConfigureEventLocation(EntityTypeBuilder<EventLocation> builder)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ConfigureCatalogEvent(EntityTypeBuilder<CatalogEvent> builder)
+        {
+            builder.ToTable("Catalog");
+            builder.Property(c => c.Id)
+                .IsRequired()
+                .ForSqlServerUseSequenceHiLo("catalog_hilo");
+            builder.Property(c => c.Title)
+                .IsRequired()
+                .HasMaxLength(100);
+            builder.HasOne(c => c.EventLocation)
+                .WithMany()
+                .HasForeignKey(c => c.LocationId);
+            builder.Property(c => c.ShowMap);
+            builder.Property(c => c.StartDateTime);
+            builder.Property(c => c.EndDateTime);
+            builder.Property(c => c.ImageUrl);
+            builder.Property(c => c.Description);
+            builder.Property(c => c.OrganizerName)
+                .HasMaxLength(100);
+            builder.Property(c => c.OrganizerDescription);
+            builder.Property(c => c.FacebookLink);
+            builder.Property(c => c.TwitterLink);
+            builder.HasOne(c => c.EventType)
+                .WithMany()
+                .HasForeignKey(c => c.EventTypeId);
+            builder.HasOne(c => c.EventCategory)
+                .WithMany()
+                .HasForeignKey(c => c.EventCatagoryId);
+            builder.Property(c => c.Price);
+
+        }
+
+        private void ConfigureCatalogType(EntityTypeBuilder<CatalogType> builder)
+        {
+            builder.ToTable("CatalogType");
+            builder.Property(c=>c.Id)
+                .IsRequired()
+                .ForSqlServerUseSequenceHiLo("catalog_type_hilo");
+            builder.Property(t => t.Type)
+                .IsRequired()
+                .HasMaxLength(100);
+        }
+
+        private void ConfigureCatalogCategory(EntityTypeBuilder<CatalogCategory> builder)
+        {
+            builder.ToTable("CatalogCategory");
+            builder.Property(c => c.Id)
+                .IsRequired()
+                .ForSqlServerUseSequenceHiLo("catalog_category_hilo");
+            builder.Property(t => t.Category)
+                .IsRequired()
+                .HasMaxLength(100);
+        }
+    }
+}
---
+++ EventCatalog/Data/CatalogSeed.cs
@@ -0,0 +1,98 @@
+﻿using EventCatalogAPI.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventCatalogAPI.Data
+{
+    public static class CatalogSeed
+    {
+        public static void Seed(CatalogContext context)
+        {
+            context.Database.Migrate();
+            if (!context.CatalogCategories.Any())
+            {
+                context.CatalogCategories.AddRange(GetPreConfiguredCatalogCategories());
+                context.SaveChanges();
+            }
+            if (!context.CatalogTypes.Any())
+            {
+                context.CatalogTypes.AddRange(GetPreConfiguredCatalogTypes());
+                context.SaveChanges();
+            }
+            if (!context.CatalogEvents.Any())
+            {
+                context.CatalogEvents.AddRange(GetPreConfiguredCatalogEvents());
+                context.SaveChanges();
+            }
+            if (!context.)
+            {
+
+            }
+        }
+
+        private static IEnumerable<CatalogEvent> GetPreConfiguredCatalogEvents()
+        {
+            return new List<CatalogEvent>
+            {
+                new CatalogEvent{ Title="TEDxSeattle 2019",                }
+
+            };
+        }
+
+        private static IEnumerable<CatalogType> GetPreConfiguredCatalogTypes()
+        {
+            return new List<CatalogType>
+            {
+                new CatalogType { Type = "Appearance or Singing" },
+                new CatalogType { Type = "Attraction" },
+                new CatalogType { Type = "Camp, trip, or Retreat" },
+                new CatalogType { Type = "Class, Training, or Workshop" },
+                new CatalogType { Type = "Concert or Performance" },
+                new CatalogType { Type = "Conference" },
+                new CatalogType { Type = "Convention" },
+                new CatalogType { Type = "Dinner or Gala" },
+                new CatalogType { Type = "Festival or Fair" },
+                new CatalogType { Type = "Game or Competition" },
+                new CatalogType { Type = "Meeting or Networking Event" },
+                new CatalogType { Type = "Other" },
+                new CatalogType { Type = "Party or Social Gathering" },
+                new CatalogType { Type = "Race or Endurance Event" },
+                new CatalogType { Type = "Rally" },
+                new CatalogType { Type = "Screening" },
+                new CatalogType { Type = "Seminar or Talk" },
+                new CatalogType { Type = "Tour" },
+                new CatalogType { Type = "Tournament" }
+            };
+        }
+
+        private static IEnumerable<CatalogCategory> GetPreConfiguredCatalogCategories()
+        {
+            return new List<CatalogCategory>
+            {
+                new CatalogCategory{ Category="Business or Profession"},
+                new CatalogCategory{ Category="Charity & causes"},
+                new CatalogCategory{ Category="Community & Culture"},
+                new CatalogCategory{ Category="Family & Education"},
+                new CatalogCategory{ Category="Fashion & Beauty"},
+                new CatalogCategory{ Category="Film, Media & Entertainment"},
+                new CatalogCategory{ Category="Food & Drink"},
+                new CatalogCategory{ Category="Government & Politics"},
+                new CatalogCategory{ Category="Health & Wellness"},
+                new CatalogCategory{ Category="Hobbies & Special Interest"},
+                new CatalogCategory{ Category="Home & Lifestyle"},
+                new CatalogCategory{ Category="Music"},
+                new CatalogCategory{ Category="Other"},
+                new CatalogCategory{ Category="Performing & Visual Arts"},
+                new CatalogCategory{ Category="Religion & Spirituality"},
+                new CatalogCategory{ Category="School Activities"},
+                new CatalogCategory{ Category="Science & Technology"},
+                new CatalogCategory{ Category="Seasonal & Holiday"},
+                new CatalogCategory{ Category="Sports & Fitness"},
+                new CatalogCategory{ Category="Travel & Outdooor"}
+            };
+        }
+    }
+}
