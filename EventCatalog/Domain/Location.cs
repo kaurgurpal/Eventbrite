810616bd6c2ae786482fd544cc708327ddecbac6
Gurpal Kaur <kaurgurpal@users.noreply.github.com>
2019-09-19 00:34:58 -0700

    Gurpal (#8)
    * Added Domain Classes and Data Context
    
    Seeding in progress
    
    * Made Location changes
    Committing Location.cs and CatalogSeed.cs
    
    * Changed made to CatalogContext.cs for ConfigureLocation method
    
    * deleted redundant files
    
    * Deleted Files

--- EventCatalog/Domain/Location.cs
+++ EventCatalog/Domain/Location.cs
@@ -8,10 +8,13 @@ namespace EventCatalogAPI.Domain
     public class Location
     {
         public int Id { get; set; }
+        public int UserId { get; set; }
+        public string VenueName { get; set; }
         public string Address { get; set; }
+        public string Address2 { get; set; }
         public string City { get; set; }
         public string State { get; set; }
         public int PostalCode { get; set; }
-
+        public string Country { get; set; }
     }
 }
