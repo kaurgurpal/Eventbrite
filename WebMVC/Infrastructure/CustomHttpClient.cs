b57b57424e5441d58787a537d2c21ed32adadf18
Sujata <sujata.tamabt12@gmail.com>
2019-10-25 00:12:46 -0700

    Fix build break after merge

--- WebMVC/Infrastructure/CustomHttpClient.cs
+++ WebMVC/Infrastructure/CustomHttpClient.cs
@@ -110,45 +110,6 @@ namespace WebMVC.Infrastructure
             return response;
         }
 
-        private async Task<HttpResponseMessage> DoPostPutAsync<T>(HttpMethod method, string uri, T item, string autherizationToken, string autherizationMethod)
-        {
-            if (method != HttpMethod.Post && method != HttpMethod.Put)
-            {
-                throw new ArgumentException("Value must be either post or put.", nameof(method));
-            }
-
-            // a new StringContent must be created for each retry 
-            // as it is disposed after each call
-
-            var requestMessage = new HttpRequestMessage(HttpMethod.Post, uri);
-
-            requestMessage.Content = new StringContent(JsonConvert.SerializeObject(item), System.Text.Encoding.UTF8, "application/json");
-            //  SetAuthorizationHeader(requestMessage);
-            if (autherizationToken != null)
-            {
-
-
-                requestMessage.Headers.Authorization = new AuthenticationHeaderValue(autherizationMethod, autherizationToken);
-
-            }
-
-
-            //if (requestId != null)
-            //{
-            //    requestMessage.Headers.Add("x-requestid", requestId);
-            //}
-
-            var response = await _client.SendAsync(requestMessage);
-
-            // raise exception if HttpResponseCode 500 
-            // needed for circuit breaker to track fails
-
-            if (response.StatusCode == HttpStatusCode.InternalServerError)
-            {
-                throw new HttpRequestException();
-            }
-
-            return response;
-        }
+        
     }
 }
